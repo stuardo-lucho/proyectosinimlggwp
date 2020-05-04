715cb74a3c49d661b6942b927ec94c5b8bd8bb75
Stuardo <stuardo.lucho@gmail.com>
2020-05-04 12:39:10 -0500

    proyecto base

---
+++ src/test/java/com/example/proyectosinimlggwp/ProyectosinimlggwpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.proyectosinimlggwp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectosinimlggwpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
