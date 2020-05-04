3a525531ea02c4ef282fb09cfb518f439d88bcd2
Stuardo <stuardo.lucho@gmail.com>
2020-05-04 12:54:30 -0500

    index

---
+++ src/main/java/com/example/proyectosinimlggwp/controllers/indexController.java
@@ -0,0 +1,15 @@
+package com.example.proyectosinimlggwp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class indexController {
+
+    @GetMapping("")
+    public String index(){
+        return "index";
+    }
+
+}
